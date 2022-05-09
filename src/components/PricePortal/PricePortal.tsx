dc7fc9d960c1d391f31d05f8249e67ffb28c7451
goudete <goudetenrique@gmail.com>
2022-05-08 18:21:27 -0700

    minor tweaks

--- src/components/PricePortal/PricePortal.tsx
+++ src/components/PricePortal/PricePortal.tsx
@@ -16,6 +16,9 @@ type Props = {
 function PricePortal(props: Props) {
     const { priceAnalysis, currentPriceAnalysis } = props;
 
+    const GREEN_HEX = "#27f8c2";
+    const BLUE_HEX = "#42c7ff";
+
     return (
         <div className='pricePortal'>
             <Card className='pricePortal__chart' interactive={true} elevation={Elevation.FOUR}>
@@ -25,8 +28,8 @@ function PricePortal(props: Props) {
                     data={priceAnalysis}
                     margin={{ top: 5, right: 25, left: 30, bottom: 5 }}
                 >
-                    <Line type="monotone" dataKey="orcaSolUsdtSpotPrice" stroke="#4ecead" strokeWidth={4} />
-                    <Line type="monotone" dataKey="orcaSolUsdcSpotPrice" stroke="#7dcbeb" strokeWidth={4} />
+                    <Line type="monotone" dataKey="orcaSolUsdtSpotPrice" stroke={GREEN_HEX} strokeWidth={4} />
+                    <Line type="monotone" dataKey="orcaSolUsdcSpotPrice" stroke={BLUE_HEX} strokeWidth={4} />
                     <XAxis stroke="white">
                         <Label value="Seconds" position="insideBottom" dy={10} style={{ fill: 'white' }} />
                     </XAxis>
@@ -36,13 +39,13 @@ function PricePortal(props: Props) {
             <Card className='pricePortal__stats' interactive={true} elevation={Elevation.FOUR}>
                 <div className='pricePortal__stats-item'>
                     <h2>PRICES</h2>
-                    <h3 style={{ color: "#4ecead" }}>Orca SOL x USDT: ${currentPriceAnalysis.orcaSolUsdtSpotPrice}</h3>
-                    <h3 style={{ color: "#7dcbeb" }}>Orca SOL x USDC: ${currentPriceAnalysis.orcaSolUsdcSpotPrice}</h3>
+                    <h3 style={{ color: GREEN_HEX }}>Orca SOL x USDT: ${currentPriceAnalysis.orcaSolUsdtSpotPrice}</h3>
+                    <h3 style={{ color: BLUE_HEX }}>Orca SOL x USDC: ${currentPriceAnalysis.orcaSolUsdcSpotPrice}</h3>
                 </div>
                 <div className='pricePortal__stats-item'>
                     <h2>STATS</h2>
                     <h3>Arbitrage %: {currentPriceAnalysis.arbitrageOpportunityPctg}</h3>
-                    <h3>Coefficient: {currentPriceAnalysis.arbitrageOpportunity}</h3>
+                    <h3>Price Diff: ${Math.abs(currentPriceAnalysis.orcaSolUsdtSpotPrice - currentPriceAnalysis.orcaSolUsdcSpotPrice)}</h3>
                 </div>
             </Card>
         </div>
